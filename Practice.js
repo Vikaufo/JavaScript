a81f7d33f2b868047dfd2dc49af713557d040f4a
Viktoria <vika.koberman@gmail.com>
2022-04-13 11:54:39 +0300

    Add file

---
+++ Practice.js
@@ -0,0 +1,38 @@
+let Vika1 = "Hello";
+let Vika2 = 50;
+let n1 = 10,
+  n2 = 30,
+  Vika3 = "100",
+  n4 = 200;
+
+let type = typeof Vika3;
+
+let summBody = +Vika3 + n4;
+// let summBody = perseInt(Vika3) + n4;
+let type1 = typeof summBody;
+// console.log(summBody)
+// console.log(type1)
+
+let boolTrue = true;
+let boolFalse = false;
+
+let b1 = boolTrue + boolFalse;
+// console.log(b1)
+// console.log(typeof(b1))
+
+let compare1 = n1 == n2;
+let compare2 = n1 <= n2;
+// console.log(compare1)
+// console.log(compare2)
+
+if (compare1) {
+  let t1 = 5;
+  let t2 = 10;
+  let result = t1 + t2;
+  console.log("IF_RESULT = " + result);
+} else {
+  // console.log("IF_RESULT = " + compare1);
+}
+
+for (let ii = 0; ii <= 10; ii++) {}
+// console.log(ii)
